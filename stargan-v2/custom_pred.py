6aa45df9f442b3c78d8b62d86e535e44088ee40c
kdhsimplepro <kdhsimplepro@gmail.com>
2023-08-08 01:19:02 +0900

    test

--- stargan-v2/custom_pred.py
+++ stargan-v2/custom_pred.py
@@ -16,10 +16,12 @@ from PIL import Image
 
 from preprocessing import Preprocessing
 
+import matplotlib.pyplot as plt
+
 img_size = 256
 style_dim = 64
 latent_dim = 16
-w_hpf = 1e-8
+w_hpf = 0
 num_domains = 2
 
 generator = nn.DataParallel(Generator(img_size, style_dim, w_hpf=w_hpf))
@@ -32,20 +34,22 @@ style_encoder_ema = copy.deepcopy(style_encoder)
 
 nets_ema = Munch(generator=generator_ema, mapping_network=mapping_network_ema, style_encoder=style_encoder_ema)
 
-fan = nn.DataParallel(FAN(fname_pretrained="expr/checkpoints/wing.ckpt").eval())
-fan.get_heatmap = fan.module.get_heatmap
-nets_ema.fan = fan
+# fan = nn.DataParallel(FAN(fname_pretrained="expr/checkpoints/wing.ckpt").eval())
+# fan.get_heatmap = fan.module.get_heatmap
+# nets_ema.fan = fan
 
 
-ckptios = CheckpointIO('expr2/checkpoints/100000_nets_ema.ckpt', data_parallel=True, **nets_ema)
+ckptios = CheckpointIO('expr5/checkpoints/100000_nets_ema.ckpt', data_parallel=True, **nets_ema)
 ckptios.load(100000)
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 y_ref = 1 # forehead0: 0, forehead1: 1
 
-src_path = "../data/augmentation_data/forehead/valid/0/80000.jpg"
-ref_path = "../data/augmentation_data/forehead/valid/1/80005.jpg"
+# src_path = "../data/augmentation_data/forehead/valid/0/80000.jpg"
+# src_path = "/home/kdhsimplepro/Pictures/Screenshots/kdh_face.png"
+src_path = "./V.png"
+ref_path = "../data/augmentation_data/forehead/valid/1/80003.jpg"
 
 transform = TF.Compose([
         TF.Resize((256, 256)),
@@ -56,8 +60,6 @@ transform = TF.Compose([
 preprocessing = Preprocessing(device="cuda")
 preprocessed_ref = preprocessing.preprocess_(src_path=src_path, ref_path=ref_path)
 
-import matplotlib.pyplot as plt
-
 plt.imshow(preprocessed_ref)
 plt.show()
 
@@ -66,16 +68,18 @@ x_ref = transform(Image.open(ref_path)).unsqueeze(0).to(device)
 
 s_ref = nets_ema.style_encoder(x_ref, y_ref)
 
-x_src = transform(Image.open(src_path)).unsqueeze(0).to(device)
+x_src = transform(Image.open(src_path).convert("RGB")).unsqueeze(0).to(device)
 
 def denormalize(x):
     out = (x + 1) / 2
     return out.clamp_(0, 1)
 
-masks = nets_ema.fan.get_heatmap(x_src)
-fake = denormalize(nets_ema.generator(x_src, s_ref, masks=masks))
+# masks = nets_ema.fan.get_heatmap(x_src)
+# fake = denormalize(nets_ema.generator(x_src, s_ref, masks=masks))
+fake = denormalize(nets_ema.generator(x_src, s_ref, masks=None))
 fake_img = TF.ToPILImage()(fake.cpu().detach().squeeze(0))
 
 print(fake)
 print(fake.shape)
-fake_img.save("./test1.png")
+fake_img.save("./v_test.png")
+# fake_img.show()
