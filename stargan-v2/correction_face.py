917108a21493867067fc1ee8ae1b533173336cd2
kdhsimplepro <kdhsimplepro@gmail.com>
2023-08-15 10:32:30 +0900

    modified

--- stargan-v2/correction_face.py
+++ stargan-v2/correction_face.py
@@ -1,6 +1,6 @@
 import torch
 from torch.nn import MSELoss
-from torch.optim import Adam
+from torch import optim
 
 from PIL import Image
 
@@ -14,7 +14,7 @@ from models import UNet
 
 unet = UNet().to("cuda")
 
-unet.load_state_dict(torch.load("/home/kdhsimplepro/kdhsimplepro/AI/Stylers/UNet/best_params.pth"))
+unet.load_state_dict(torch.load("/home/kdhsimplepro/kdhsimplepro/AI/Stylers/UNet/male_female_best_params.pth"))
 unet.eval()
 
 @torch.no_grad()
@@ -23,8 +23,8 @@ def pred_unet(x):
     mask = unet(x)
     return mask
 
-original_path = "./V.png"
-transformed_path = "./v_test.png"
+original_path = "./jungwoon/mirror.png"
+transformed_path = "./jungwoon/mirror_test.png"
 
 original_img = TF.to_tensor(Image.open(original_path).resize((256, 256)).convert("RGB")).to("cuda").unsqueeze(0)
 transformed_img = TF.to_tensor(Image.open(transformed_path).resize((256, 256)).convert("RGB")).to("cuda").unsqueeze(0)
@@ -33,7 +33,8 @@ transformed_img.requires_grad = True
 original_mask = pred_unet(original_img)
 transformed_mask = pred_unet(transformed_img).detach()
 
-optim = Adam([transformed_img], lr=0.002)
+optimizer = optim.Adam([transformed_img], lr=0.1)
+scheduler = optim.lr_scheduler.LambdaLR(optimizer=optimizer, lr_lambda=lambda epoch: 0.95 ** epoch, last_epoch=-1)
 
 label = original_img * (1 - original_mask)
 
@@ -64,29 +65,25 @@ for i in range(256):
 def get_gaussian_dist(x, y, size=256):
     matrix = torch.zeros((size, size))
 
-    dist = torch.distributions.normal.Normal(torch.tensor([0]), torch.tensor([size])/2)
+    dist = torch.distributions.normal.Normal(torch.tensor([0]), torch.tensor([size/2]))
     distance = torch.sum(torch.abs(coordinates - torch.Tensor([y, x])), dim=2)
     matrix = torch.exp(dist.log_prob(distance))
     matrix /= torch.sum(matrix)
     
     return matrix
 
-gaussian_matrix = torch.zeros((256, 256))
+gaussian_matrix_dict = {}
+weight_dict = {}
 
 for i in range(256):
     for j in range(256):
         if interpolated_mask[0, i, j] >= 0.7:
-            gaussian_matrix += get_gaussian_dist(i, j, 256)
-
-gaussian_matrix /= torch.sum(gaussian_matrix)
-gaussian_matrix = gaussian_matrix.to("cuda")
-import matplotlib.pyplot as plt
-plt.imshow(gaussian_matrix.cpu().detach().permute(1, 0).numpy().reshape(256, 256, 1), cmap="gray")
-plt.show()
+            gaussian_matrix_dict[(i, j)] = get_gaussian_dist(i, j, 256).to("cuda")
+            weight_dict[(i, j)] = transformed_img[:, :, i, j] / (torch.sum(transformed_img * gaussian_matrix_dict[(i, j)].reshape(1, 1, 256, 256).repeat(1, 3, 1, 1)))
 
 interpolated_img = interpolated_img.squeeze(0)
 
-alpha = 0.5
+alpha = 0.01
 for k in range(100):
 
     with torch.no_grad():
@@ -94,22 +91,25 @@ for k in range(100):
 
     loss = criterion(transformed_img, interpolated_img.unsqueeze(0))
 
-    optim.zero_grad()
+    optimizer.zero_grad()
     loss.backward(retain_graph=True)
     with torch.no_grad():
         mean_grad = torch.mean(transformed_img.grad)
-        expectation_grad = torch.sum(transformed_img.grad * gaussian_matrix.reshape(1, 1, 256, 256).repeat(1, 3, 1, 1))
-        print(mean_grad,expectation_grad)
+        
+        for i in range(256):
+            for j in range(256):
+                transformed_img.grad[:, :, i, j] = (alpha * transformed_img.grad[:, :, i, j] + (1-alpha)*mean_grad)
+
+    
+    optimizer.step()
+    scheduler.step()
+
+    with torch.no_grad():
         for i in range(256):
             for j in range(256):
                 if interpolated_mask[:, i, j] >= 0.7:
-                    transformed_img.grad[:, :, i, j] = expectation_grad
-                
-                else:
-                    transformed_img.grad[:, :, i, j] = (alpha * transformed_img.grad[:, :, i, j] + (1-alpha)*mean_grad)
-
-    optim.step()
+                    transformed_img[:, :, i, j] = weight_dict[(i, j)] * (torch.sum(transformed_img * gaussian_matrix_dict[(i, j)].reshape(1, 1, 256, 256).repeat(1, 3, 1, 1)))
 
     print(k, round(loss.item(), 4))
 
-make_gif(imgs, save_path="./corrected_face_v.gif")
+make_gif(imgs, save_path="./jungwoon/corrected_face_mirror.gif")
